5b80d14967491bffa80f79bd870288a4b7e44e57
Leah <lepa0001@stud.kea.dk>
2024-10-08 09:35:45 +0200

    .

--- Theme_switcher/script.js
+++ Theme_switcher/script.js
@@ -1,17 +1,17 @@
 document.getElementById("theme-selector").addEventListener("change", function () {
-  document.body.setAttribute("data-theme", this.value);
-});
-
-document.getElementById("theme-selector").addEventListener("change", function () {
   var theme = this.value;
-  var logo = document.getElementById("logo");
+  var purpleElement = document.getElementById("purple");
 
-  if (theme === "light") {
-    logo.src = "../logo.svg";
-  } else if (theme === "dark") {
-    logo.src = "../logo_dark.svg";
-  } else if (theme === "hawaii") {
-    logo.src = "../logo_hawaii.svg";
+  // Skift styling af "purple" elementet baseret på tema
+  if (purpleElement) {
+    var styles = {
+      light: { backgroundColor: "lightgray", color: "black" },
+      dark: { backgroundColor: "black", color: "white" },
+      purple: { backgroundColor: "purple", color: "white" },
+    };
+    Object.assign(purpleElement.style, styles[theme]);
   }
+
+  // Ændr temaet for body elementet
   document.body.setAttribute("data-theme", theme);
 });
